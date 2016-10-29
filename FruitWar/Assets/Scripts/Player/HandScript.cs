a1c2c6c0c5e443b120a8009826b1e536c1020878
Kevin <alu0100789461@ull.edu.es>
2016-10-29 14:24:03 +0100

    Cambios para adaptar al mando

--- FruitWar/Assets/Scripts/Player/HandScript.cs
+++ FruitWar/Assets/Scripts/Player/HandScript.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class HandScript : MonoBehaviour {
 	private Transform parentWeaponSelected;
@@ -25,4 +26,17 @@ public class HandScript : MonoBehaviour {
 		weapon.localRotation = rotationWeapon;
 		weapon.localScale = scaleWeapon;
 	}
+
+	/*void FixedUpdate() {
+	 // Hacer la funcion hit() static
+		if (CrossPlatformInputManager.GetAxis ("Fire1") == 1) {
+			SwordHit.hit ();
+		}
+		if (CrossPlatformInputManager.GetAxis ("Fire2") == 1) {
+			SwordHit.hit ();
+		}
+		if (CrossPlatformInputManager.GetAxis ("Fire3") == 1) {
+			SwordHit.hit ();
+		}
+	}*/
 }
