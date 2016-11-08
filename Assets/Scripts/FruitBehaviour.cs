8d21fe0faf6cbaee6db415eda02d6caf582b5901
teguayco <alu0100825503@ull.edu.es>
2016-11-08 18:29:57 +0200

    solucionado error generación fruta aleatoria

---
+++ Assets/Scripts/FruitBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Script asociado a cada fruta de la casa
+ */
+public class FruitBehaviour : MonoBehaviour {
+	public GameObject fruitBaseAttached;
+	private bool colision = false;
+	public bool cutted = false;
+	public GameObject colisionador = null;
+
+	void Update () {
+		if (!cutted) {
+			gameObject.transform.Rotate (new Vector3 (0f, 1f, 0f) * 50 * Time.deltaTime);
+		}
+	}
+
+	public void setFruitBaseAttached(GameObject aFruitBase) {
+		fruitBaseAttached = aFruitBase;
+	}
+
+	public GameObject getBaseAttached() {
+		return fruitBaseAttached;
+	}
+
+	void OnCollisionEnter(Collision obj) {
+		colisionador = obj.gameObject;
+
+		if(!colision) {
+			colision = true;
+			if (colisionador.tag == "Fruit") {
+				Invoke ("destroyFruit", 3f);
+			} else {
+				Invoke ("destroyFruit", 10f);
+			}
+		}
+	}
+
+	public void destroyFruit() {
+		fruitBaseAttached.GetComponent<BaseScript> ().setNoFruit ();
+		Destroy (gameObject);
+		if (colisionador.tag == "Fruit") {
+			Destroy (colisionador);
+		}
+	}
+}
---
+++ Assets/Scripts/RandomFruitsGenerator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RandomFruitsGenerator : MonoBehaviour {
+	public GameObject[] fruits;
+
+	public GameObject[] getFruits() {
+		return fruits;
+	}
+}
